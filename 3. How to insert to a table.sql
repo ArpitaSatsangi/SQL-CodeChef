4bfe8403714d2b58f9c6c73af6b1263adbb41051
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-08-01 08:39:27 +0530

    Create 3. How to insert to a table.sql

---
+++ 3. How to insert to a table.sql
@@ -0,0 +1,7 @@
+/* Write a query to add the below mentioned employee details to the EMPLOYEE table.
+Employee id: 06, Employee Name: Brandon Kim, Department: Operations
+*/
+
+INSERT INTO Employee( Employee_id, Employee_Name , Department)
+  
+VALUES( 6, 'Brandon Kim', 'Operations');
