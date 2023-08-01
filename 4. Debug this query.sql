f1b60c7d8f68fe93a475e234dca500a65288d002
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-08-01 08:41:46 +0530

    Create 4. Debug this query.sql

---
+++ 4. Debug this query.sql
@@ -0,0 +1,14 @@
+/* The Query written in the console is trying to insert data to the table employee.
+Your table is named 'employee' and has the following columns:
+Id (INT)
+Name (TEXT),
+Age (INT),
+Address (TEXT)
+
+Debug this query to output the entire table */
+
+INSERT INTO employee (Id,Name,Age,Address)
+VALUES  (1, 'John Smith', 25,  '123 Main St'),
+        (2, 'Sarah Johnson', 30,'456 Broadway');
+
+SELECT * FROM employee; 
