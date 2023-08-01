67a5afeb2c9e9376f67228370c1db8a047ac3ad7
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-08-01 08:21:54 +0530

    Create 1. Create table.sql

---
+++ 1. Create table.sql
@@ -0,0 +1,9 @@
+/* Write a query to create a table 'employee', with columns employee_id, employee_Name and Department. 
+Update the blanks below to solve this problem */
+
+CREATE TABLE employee
+(
+    Employee_id INT , /* Integer Data type assigned to the variable */
+    Employee_Name TEXT, /* TEXT Data type assigned to the variable */
+    Department TEXT /* TEXT Data type assigned to the variable */
+);
