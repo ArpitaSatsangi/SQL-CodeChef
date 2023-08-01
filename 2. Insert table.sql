7fd21751bb46395e38d2b2d1fb28145be2305421
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-08-01 08:37:13 +0530

    Create 2. Insert table.sql

---
+++ 2. Insert table.sql
@@ -0,0 +1,7 @@
+/* Write a query to insert the below mentioned employee details to the table 'employee' */
+
+INSERT INTO employee(Employee_id, Employee_Name, Department)
+VALUES (4,'Marcus Garcia','Product');
+
+INSERT INTO employee(Employee_id, Employee_Name, Department)
+VALUES (5,'Samantha Park','Hr');
